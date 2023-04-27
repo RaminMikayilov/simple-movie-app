b6b62332b0fa3038134f7c94955722b669abc130
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-04-27 18:43:48 +0400

    Update Hero and Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "425px",
+      md: "768px",
+      lg: "976px",
+      xl: "1220px",
+    },
     extend: {
       colors: {
         cRose: "#BE123C",
