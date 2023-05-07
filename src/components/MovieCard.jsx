b12c8169fd3c94cd30d3a088487ef747ad75ed50
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-05-07 20:47:41 +0400

    Fix nesting problem

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,30 +14,31 @@ const MovieCard = (movie) => {
   const [favorite, setFavorite] = useState(false);
 
   return (
-    <div className="inline-block w-[200px] h-[470px] rounded-sm bg-cCard shadow-md shadow-cRose/20 duration-300 relative">
-      {/* favorite */}
-      <span
-        className="absolute right-2 top-2 bg-white/60 text-cRose rounded-full p-1 z-[15] hover:bg-white/100 cursor-pointer"
-        onClick={() => {
-          dispatch(toggleFavorite(movie));
-          setFavorite(!favorite);
-        }}
-      >
-        {movie.isFavorite || favorite ? (
-          <AiFillHeart size={25} />
-        ) : (
-          <AiOutlineHeart size={25} />
-        )}
-      </span>
+    <Link to={`/movies/${id}`}>
+      <div className="inline-block w-[200px] h-[470px] rounded-sm bg-cCard shadow-md shadow-cRose/20 relative hover:opacity-60 duration-300">
+        {/* favorite */}
+        <span
+          className="absolute right-2 top-2 bg-white/60 text-cRose rounded-full p-1 z-[15] hover:bg-white/100 cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(toggleFavorite(movie));
+            setFavorite(!favorite);
+          }}
+        >
+          {movie.isFavorite || favorite ? (
+            <AiFillHeart size={25} />
+          ) : (
+            <AiOutlineHeart size={25} />
+          )}
+        </span>
 
-      <img
-        src={`https://image.tmdb.org/t/p/original${poster_path}`}
-        alt={title}
-        className="rounded-r-sm rounded-l-sm w-full"
-      />
-      {/* card body */}
-      <Link to={`/movies/${id}`}>
-        <div className="p-3 space-y-2 w-full cursor-pointer hover:opacity-60 duration-300">
+        <img
+          src={`https://image.tmdb.org/t/p/original${poster_path}`}
+          alt={title}
+          className="rounded-r-sm rounded-l-sm w-full"
+        />
+        {/* card body */}
+        <div className="p-3 space-y-2 w-full cursor-pointer">
           <p className="text-cYellow font-medium py-1">{release_date}</p>
           <h1 className="text-white font-bold text-lg w-full whitespace-normal h-[60px]">
             {title}
@@ -47,8 +48,8 @@ const MovieCard = (movie) => {
             <span className="font-bold text-lg">{vote_average}</span>
           </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
