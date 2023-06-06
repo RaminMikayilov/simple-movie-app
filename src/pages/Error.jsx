b18e02c8fc3c093763b9ba7f1596a0bf2bc510e3
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-06-06 12:12:04 +0400

    Update Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -9,8 +9,8 @@ const Error = () => {
     <div className="text-cyan-200 h-screen w-full flex flex-col items-center justify-center relative">
       <div className="border-cRose text-center border-4 p-4 md:p-7 shadow-lg shadow-cRose bg-cCard">
         <h1 className="md:text-9xl text-7xl md:p-8 p-6 ">404</h1>
+        <h2 className="md:text-4xl text-2xl md:p-8 p-6">Page not found</h2>
         <Link to="/">
-          <h2 className="md:text-4xl text-2xl md:p-8 p-6">Page not found</h2>
           <div className="flex justify-center items-center border-2 p-2 gap-1 cursor-pointer hover:bg-cBlue duration-300 rounded-full">
             Back
             <AiOutlineHome />
